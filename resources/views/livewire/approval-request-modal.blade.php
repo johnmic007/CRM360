930ad05e1be94c61a2c1213471da03e6a61f1309
john micheal <johnmichealpprince@gmail.com>
2024-12-20 13:39:12 +0530

    Update approval-request-modal.blade.php

--- resources/views/livewire/approval-request-modal.blade.php
+++ resources/views/livewire/approval-request-modal.blade.php
@@ -1,22 +1,3 @@
 <div>
-    <form wire:submit.prevent="submit">
-        <div>
-            <x-filament::card>
-                <x-filament::form>
-                    <x-filament::form.field label="Message to Manager">
-                        <textarea wire:model.defer="message" 
-                                  class="w-full border-gray-300 rounded-md" 
-                                  rows="5" 
-                                  placeholder="Write your message to the manager here..."></textarea>
-                        @error('message') <span class="text-red-600">{{ $message }}</span> @enderror
-                    </x-filament::form.field>
-                </x-filament::form>
-            </x-filament::card>
-        </div>
-        <div class="mt-4 flex justify-end">
-            <x-filament::button type="submit" class="bg-primary-500 text-white">
-                Send Request
-            </x-filament::button>
-        </div>
-    </form>
+
 </div>
